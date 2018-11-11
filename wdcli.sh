1a48fd482c7a0667028ffe0ef99907c7c5eb859f
RaimiHakim1141 <raimihakim1141@gmail.com>
2018-11-11 22:42:32 +0800

    Edited wdcli file
    Added more core download mechanism
    
    Signed-off-by: RaimiHakim1141 <raimihakim1141@gmail.com>

--- wdcli.sh
+++ wdcli.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+suburl='wiki/latest/'
+cat locales
+read -p "What locales or languages do you want to use? " locales
+for url in $(cat url);do
+	for locale in $locales; do
+		parses=($(curl $url$locale$suburl | awk '{print $9}'))
+		echo ${parses[0]}
+		wget  $url$locale$suburl${parses[0]}
+	done
+done
