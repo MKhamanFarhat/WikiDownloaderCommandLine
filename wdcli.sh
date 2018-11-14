7489b8887eba711c672792e13bddc6f432fb2e11
Farbafe <kedy6yar@nottingham.edu.my>
2018-11-14 09:08:16 +0800

    Adds log file to wget and fixes typing mistake.

---
+++ wdcli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+suburl='wiki/latest/'
+logfile='log.txt'
+cat locales
+read -p "What locales or languages do you want to use? " locales
+for url in $(cat url);do
+	for locale in $locales; do
+		parses=($(curl $url$locale$suburl | awk '{print $9}'))
+		echo ${parses[0]}
+		wget  -a $logfile $url$locale$suburl${parses[0]}
+	done
+done
